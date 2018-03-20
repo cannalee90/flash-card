fe59cc7916a4a68c7cb1e760c647e1080ffd61be
Kangho Lee <cannalee90@gmail.com>
2018-03-20 19:12:47 +0900

    remove count part

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,8 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form'
 
-const counter = (state = {}, action) => {
-  switch(action.type) {
-    case 'TEST':
-    default:
-      return state;
-  }
-}
 
 const rootReducer = combineReducers({
-  counter,
   form: formReducer,
 });
 

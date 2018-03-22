931a4ccf81c3be57e0424c60752cd6f3320d1ea2
Kangho Lee <cannalee90@gmail.com>
2018-03-22 18:55:01 +0900

    remove bindexpression

--- src/containers/NewCard.js
+++ src/containers/NewCard.js
@@ -5,13 +5,8 @@ import { withRouter } from 'react-router-dom'
 import FormTest from './form';
 
 class NewCard extends Component {
-  constructor(props) {
-    super(props);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onSubmit(values) {
-    console.log(values);
+  
+  onSubmit = (values) => {
   }
 
   render() {
@@ -20,7 +15,7 @@ class NewCard extends Component {
         <h1>This is new Card</h1>
         <FormTest
           onSubmit={this.onSubmit}
-        />        
+        />
       </div>
     );
   }

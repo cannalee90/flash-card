49498b28613ef3ea3d39272d122224255bc5419f
Kangho Lee <cannalee90@gmail.com>
2018-03-31 00:20:14 +0900

    redirect to github Oauth page

--- src/containers/signin.js
+++ src/containers/signin.js
@@ -1,30 +1,45 @@
 import React, { Component } from 'react';
+import QueryString from 'query-string';
+
 import { Link } from 'react-router-dom';
 import Config from './../config';
 import Popup from './../utils/popup';
 
-// 일단 토큰을 생성해서 gist에 접근하는 방법을 사용
-// 여러명이서 할땐 어짜피 리포를 사용해야함
-// 다음 버젼에서는 리포를 사용해서 여러명이서 할 수 있도록 한다.
-
 class Signin extends Component {
   constructor(props) {
     super(props);
-    this.githubURL = `https://github.com/login/oauth/authorize?client_id=${Config.githubAuth.clientId}`;
-    
+    const clistApp = 'b1497163e82bf1dfd9c1'
+    this.githubURL = `https://github.com/login/oauth/authorize?client_id=${clistApp}`
+    this.state = {
+      hasCodeParams: false,
+    }
   }
 
-  signin = () => {
+  requestGithub = () => {
+    window.location.href = this.githubURL;
   }
 
   componentDidMount() {
+    const { search } = this.props.location;
+    const query = QueryString.parse(search);
+    if(query['code']) {
+      this.setState({
+        hasCodeParams: true,
+      });
+
+    } else {
+      this.setState({
+        hasCodeParams: false,
+      })
+    }
   }
 
   render() {
+    const { hasCodeParams } = this.state;
+    console.log(hasCodeParams);
     return(
       <div>
-        <h1>This is signin page enter token</h1>
-        
+        {!hasCodeParams && <button onClick={this.requestGithub}>Login</button>}       
       </div>
     )
   }

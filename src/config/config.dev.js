4b0ea968427bb26da17ef7335260c2c2a0ce0a7b
Kangho Lee <cannalee90@gmail.com>
2018-04-01 16:48:03 +0900

    env value setting for config files

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -4,6 +4,14 @@ const config = {
     clientId: 'b1497163e82bf1dfd9c1',
     clientSecret: process.env.FLASHCARD_DEV_SECRET,
   },
+  firebase: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    messageSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  }
 };
 
 export default config;

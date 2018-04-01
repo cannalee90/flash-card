b27d452704984ee0a4c4279f6b6dd354ac237594
Kangho Lee <cannalee90@gmail.com>
2018-04-01 16:58:55 +0900

    remove config file console.log

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,7 +2,6 @@ import * as firebase from "firebase";
 import Config from '../config';
 
 const { firebase: firebaseConfig } = Config;
-console.log(firebaseConfig)
 
 firebase.initializeApp(firebaseConfig);
 

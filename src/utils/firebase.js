445222aef949507e21c1793a7c36a02103ebe6ee
Kangho Lee <cannalee90@gmail.com>
2018-04-01 16:49:15 +0900

    applied config file for firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,14 +1,9 @@
 import * as firebase from "firebase";
+import Config from '../config';
 
-var config = {
-  apiKey: "AIzaSyDZTG6fGgR9djR49-nL8_WTgVQMRiJy7mM",
-  authDomain: "flashcard-dev-4d446.firebaseapp.com",
-  databaseURL: "https://flashcard-dev-4d446.firebaseio.com",
-  projectId: "flashcard-dev-4d446",
-  storageBucket: "flashcard-dev-4d446.appspot.com",
-  messagingSenderId: "462011428722"
-};
+const { firebase: firebaseConfig } = Config;
+console.log(firebaseConfig)
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 export default firebase;

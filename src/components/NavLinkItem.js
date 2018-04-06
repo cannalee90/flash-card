74b3c86686a705db28c8ad6c293ce94cf8bdd762
Kangho Lee <cannalee90@gmail.com>
2018-04-06 15:06:42 +0900

    Add target property on NavLinkItem and outerLink element

--- src/components/NavLinkItem.js
+++ src/components/NavLinkItem.js
@@ -7,11 +7,12 @@ export default (props) => {
     to,
     text,
     className,
+    target,
   } = props;
   if(to.indexOf('http') !== -1 || to.indexOf('https') !== -1 || to.indexOf('www') !== -1 ) {
     return (
       <NavItem>
-        <a className={className} href={to} target={'_flashcard'} >{text}</a>
+        <a className={className} href={to} target={target}>{text}</a>
       </NavItem>
     )
   }

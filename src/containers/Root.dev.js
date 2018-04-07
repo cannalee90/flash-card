13ae196db715310bfe7949ea1206f1261841435b
Kangho Lee <cannalee90@gmail.com>
2018-04-07 22:55:27 +0900

    add alert component on react router

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -8,7 +8,9 @@ import App from './App';
 import NewCard from './NewCard';
 import List from './List';
 import Signin from './Signin';
+
 import Nav from './../components/Nav';
+import Alert from '../components/Alert';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import '../style/app.css';
@@ -17,11 +19,12 @@ const Root = ({ store }) => {
   return (
     <Provider store={store}>
       <div>
-        <Nav />
-        <Route path='/' exact component={App} />
-        <Route path='/new' component={NewCard} />
-        <Route path='/list' component={List} />
-        <Route path='/signin' component={Signin} />
+        <Nav/>
+          <Alert />
+          <Route path='/' exact component={App} />
+          <Route path='/new' component={NewCard} />
+          <Route path='/list' component={List} />
+          <Route path='/signin' component={Signin} />
         <DevTools />
       </div>
     </Provider>

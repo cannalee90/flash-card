59564e35a3c035cb03aaec4e75367ad5890f4d32
Kangho Lee <cannalee90@gmail.com>
2018-04-08 16:33:04 +0900

    remove useless import and module

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,4 @@
-import Config from '../config';
 
-const demoCards = [
-  {
-    front: 'Process',
-    back: '실행중인 프로그램으로 말할 수 있다. 디스크에 있던 프로그램들이 메모리상에 적재된 상태',
-  },
-  {
-    front: 'Thread',
-    back: '프로세스 내에서 실행되는 여러 흐름의 단위. 한개의 프로세스는 여러가지 쓰레드를 가질 수 있으며 쓰레드끼리는 자원 공유가 용이하다',
-  }
-];
-
-export const FETCH_CARDS = 'FETCH_CARDS';
 export const SAVE_ACCESS_TOKEN = 'SAVE_ACCESS_TOKEN';
 export const FETCH_USER_INFO = 'FETCH_USER_INFO';
 export const FETCH_USER_INFO_SUCCESS = 'FETCH_USER_INFO_SUCCESS';
@@ -67,13 +54,6 @@ export const fetchUserInfoError = (error) => {
     payload: {
       error,
     }
-  }
-}
-
-export const fetchCards = () => {
-  return {
-    type: FETCH_CARDS,
-    cards: demoCards,
   }
 }
 
